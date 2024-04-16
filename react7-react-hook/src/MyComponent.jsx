03f269df82fa01a8747e9755b6a005e6b77fc308
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-17 00:17:23 +0530

    name , setName

--- react7-react-hook/src/MyComponent.jsx
+++ react7-react-hook/src/MyComponent.jsx
@@ -8,15 +8,18 @@ import React, {useState} from 'react'
 function MyComponent() {
    
     const [name, setName] = useState();
+    const updateName = () => {
+        setName(" dude");
+        console.log(name);
+    }
 
     return (
-        <div> 
-            <p>Name: {}</p>
-            <button onClick={name} >set name</button>
+        <div>
+            <p>Name:{name} </p>
+            <button onClick={updateName} >set name</button>
         </div>
     );
-
-
+    
 }
 
 export default MyComponent
